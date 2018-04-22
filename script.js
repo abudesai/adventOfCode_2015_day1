58b7567b6cf85d3b9196301646d6983d65a9c043
abudesai <32407603+abudesai@users.noreply.github.com>
2018-04-22 14:31:52 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,20 +3,23 @@ const fs = require('fs');
 
 
 //------------------------------------------------------------------------------------------
-// const file = fs.readFileSync('./SantaFile.txt');
-// console.time('my_timer');
-// let floorNum = 2 * (file.length * 40.5 - (file.reduce((acc, obj) => acc + obj, 0)))
-// console.timeEnd('my_timer');
-// console.log('Floor #:', floorNum);
+const file = fs.readFileSync('./SantaFile.txt');
+console.time('my_timer');
+let floorNum = 2 * (file.length * 40.5 - (file.reduce((acc, obj) => acc + obj, 0)))
+console.timeEnd('my_timer');
+console.log('Floor #:', floorNum);
+
+
+
 
 //------------------------------------------------------------------------------------------
 
-fs.readFile('./SantaFile.txt', (err, file) => {
-	if (err) throw err;
-	
-	console.time('my_timer2');
-	let floorNum = 2 * (file.length * 40.5 - (file.reduce((acc, obj) => acc + obj, 0)))
-	console.timeEnd('my_timer2');
+// fs.readFile('./SantaFile.txt', (err, file) => {
+// 	if (err) throw err;
 
-	console.log('Floor #:', floorNum);
-})
+// 	console.time('my_timer2');
+// 	let floorNum = 2 * (file.length * 40.5 - (file.reduce((acc, obj) => acc + obj, 0)))
+// 	console.timeEnd('my_timer2');
+
+// 	console.log('Floor #:', floorNum);
+// })
