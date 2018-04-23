15d360a1a4daa7ebaca106bd612a752493045026
abudesai <32407603+abudesai@users.noreply.github.com>
2018-04-22 17:22:06 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
 const fs = require('fs');
-
+//------------------------------------------------------------------------------------------
+/*
+open paren: 40
+close paren: 41
+num_floors = 2 * (# of chars * 40.5 - (# of ups * 40 + # of downs * 41))
+*/
 
 //------------------------------------------------------------------------------------------
 const file = fs.readFileSync('./SantaFile.txt');
@@ -8,9 +13,6 @@ console.time('my_timer');
 let floorNum = 2 * (file.length * 40.5 - (file.reduce((acc, obj) => acc + obj, 0)))
 console.timeEnd('my_timer');
 console.log('Floor #:', floorNum);
-
-
-
 
 //------------------------------------------------------------------------------------------
 
